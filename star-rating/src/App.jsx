39796c348de1afe53d25f17377fc648675acfeee
Tejas-00 <tejassonawane1709@gmail.com>
2025-04-08 16:49:47 +0530

    done with satr-rating

--- star-rating/src/App.jsx
+++ star-rating/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
 
-export default App
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
